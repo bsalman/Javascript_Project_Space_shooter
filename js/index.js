ee66dbf18104dbe0d0faf2545ebd5000f0d7459d
bsalman <bsalman395850@gmail.com>
2020-04-22 23:59:13 +0200

    22.4.2020-second update

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-
+ 
     let container=document.querySelector('.container')
  ////////////////////////////////////////////////////////////////   
     //creat laser sound
@@ -14,57 +14,59 @@ window.onload = function(){
 //declering layer1 canvas
     let gameCanvas = document.querySelector('#gameCanvas')
     let context = gameCanvas.getContext('2d')
-    
-//////////////////////////////////////////
-//declering layer2 cnvas1
-    let enemyCanvas = document.querySelector('#gameCanvas2')
-    let context2 = gameCanvas.getContext('2d',{alpha:false})
-
-     context2.globalCompositeOperation = 'destination-over';
-       
-
- //////////////////////////////////////////////////////
- //declering layer3 canvas3
- let meteorCanvas = document.querySelector('#gameCanvas3')
- let context3 = gameCanvas.getContext('2d')
-    //create enemys space ships
-    
-    
  ////////////////////////////////////////////////////
-    //creat spaceship 
-    let img  = document.createElement('img')
-    img.src ='./img/ship2.png'
-    img.onload = function () {
-        let x =10
-        let y=10
-        //add Event to the space ship 
-        gameCanvas.onmousemove = (e=>{         
-           if (true) {
-            let x= e.pageX
-            let y= e.pageY
-            context.clearRect(0,0,1000,1000)
-            context.drawImage(img,40,30,300,200,x,y,40,30)
-               
-           }
-        } )
-        //caling laser shoot function + add Event 
-       document.onclick =(e=>{
-        // console.log(e);
-        LaserShoot(e.pageX,e.pageY)
-        // LaserShoot(e.pageX-5,e.pageY-5)
-        sound.currentTime =0;
-        sound.play()
-    })
-    context.globalCompositeOperation = 'source-over';
-    }
+ let gameCanvas2 = document.querySelector('#gameCanvas2')
+ let context2 = gameCanvas2.getContext('2d')
+    ///////////////////////////////////////////
  
-     //laser shoot +laser sound
- function LaserShoot(laserX,laserY) {
+  context.clearRect(0,0,1000,500)
+  spaceshipCreator('./img/ship2.png',context,sound,context,context2) 
+
+
+  enemycreator('./img/ship4.png',context2)
+
+  
+     
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+
+ //creat spaceship function 
+function spaceshipCreator(src,ctx,sound,ctx2,ctx3) {
+  console.log('hallo');
+  
+  let img  = document.createElement('img')
+img.src =src //'./img/ship2.png'
+img.onload = function () {
+    let x =10
+    let y=10
+    //add Event to the space ship 
+    gameCanvas2.onmousemove = (e=>{ 
+       if (true) {
+        let x= e.pageX
+        let y= e.pageY
+        ctx.clearRect(0,0,1000,500)
+        ctx.drawImage(img,40,30,300,200,x,y,40,30)
+           
+       }
+    } )
+ //////////////////////////////////////////////////////////////   
+//caling laser shoot function + add Event 
+   document.onclick =(e=>{
+    LaserShoot(e.pageX,e.pageY,ctx2,ctx3)
+ /////////////////////////////////////////////////////////////////////   
+  // LaserShoot(e.pageX-5,e.pageY-5,ctx2)
+    sound.currentTime =0;
+    sound.play()
+                         })
+                       }
+
+}
+/////////////////////////////////////////////////////////////////////
+//laser shoot +laser sound
+function LaserShoot(laserX,laserY,ctx,ctx3) {
+  
     let laserCaunter = laserY
-   
-    
     let laserInterval = setInterval(() => {
-        
      if (laserCaunter ==0) {
        laserCaunter= laserY
        clearInterval(laserInterval)
@@ -72,51 +74,91 @@ window.onload = function(){
      }else{
        laserCaunter-=10
      }
-     context.fillStyle ="red"
-     context.clearRect(laserX+15,laserCaunter+10,3,5)
-     context.fillRect(laserX+15,laserCaunter,3,5)
-     context.stroke();
-      }, 50);
- 
+     
+   
+     ctx.fillStyle ="red"
+     ctx.clearRect(laserX+15,laserCaunter+10,3,5)
+     ctx.fillRect(laserX+15,laserCaunter,3,5)
+     ctx.stroke();
+     explogen (laserX,laserY,enX,enY,'./img/explosion0.png',ctx)
+     if(status=='explogen'){
+      
+     }
 
-
-       
+      }, 50); 
    }
+   ////////////////////////////////////////////////////////
+  var enX =0
+  var enY =0
+   function enemycreator(src,somCtx) {
 
-   //enemys creating function 
-  //  function enemycreator(src,somCtx,enX,enY) {
-  //    let enX =0
-  //    let enY =0
-  //    if (enX<1000) {
-  //      setInterval(() => {
-  //        enX+=100
-  //        enY+=10
-  //     let enemyimg =document.createElement('img')
-  //     enemyimg.src =src
-
-  //     enemyimg.onload= function () {
-  //       somCtx.clearRect()
-  //       somCtx.drawImage(enemyimg,0,0,700,400,enX,enY,40,30)
-  //             } 
+    console.log('hi');
+     let enemyInterval= setInterval(() => {
+        // enX+=100
+    let swetsher =true
+    let enemyimg =document.createElement('img')
+        enemyimg.src =src
        
-  //    }, 1000);
-  //    }else{
-  //     enX =0
-  //    }
-     
-     
-  //  }
-
-
-
+        enY+=5
+        enX+=0// creat function to macke enemy move as cercel
+        enemyimg.onload= function () {
+          somCtx.clearRect(0,0,1000,500)
+          somCtx.drawImage(enemyimg,0,0,700,400,enX,enY,40,30)
+             } 
+      if (enY==500 && swetsher==true) {
+      enY=0
+      enX+=50 
+      }
+      else{if(enX==1000 && swetsher==true){
+        swetsher=false
+        enX-=50
+      } }
+      if(status=='explogen'){
+        enemyimg.remove()
+       
+        
+        
+       }
+    
+      
+    }, 40);
 
     
-}
+     
+    }
 
+    var status= 'notExplogen'
+    function explogen (laserX,laserY,enX,enY,src,ctx) {
+      
+      let enWidth = 40
+      let enHeight = 30
+      let laserWidth = 3
+      let laserHeight = 5
+      let topLeftCornerCheck = checkInside(enX , enY ,enWidth, enHeight, laserX, laserY)
 
+      let topRightCornerCheck = checkInside(enX , enY ,enWidth, enHeight, laserX + laserWidth, laserY)
+      let buttomRightCornerCheck = checkInside(enX , enY ,enWidth, enHeight, laserX + laserWidth, laserY + laserHeight)
+      let buttomLeftCornerCheck = checkInside(enX , enY ,enWidth, enHeight, laserX , laserY + laserHeight)
 
+      if(topLeftCornerCheck || topRightCornerCheck || buttomRightCornerCheck || buttomLeftCornerCheck){
+        console.log('explogen');
+        status = 'explogen'
+        let img  = document.createElement('img')
+        img.src =src
+        img.onload = function () {
+         ctx.clearRect(0,0,1000,500) 
+        ctx.drawImage(img,0,0,150,100,enX,enY,70,50)
+          
+        }
+        
 
-  
+      }
+
+      
+    }
+    function checkInside(enX , enY ,enWidth, enHeight, pointX , pointY ){
+      return (pointX >= enX && pointX <= enX + enWidth) && (pointY >= enY && pointY <= enY + enHeight )
+    }
 
 
       
--- js/index2.js
+++ js/index2.js
@@ -9,42 +9,63 @@ window.onload = function(){
    //declering layer1 canvas
        let gameCanvas = document.querySelector('#gameCanvas')
        let context = gameCanvas.getContext('2d')
-       enemycreator('./img/ship4.png',context)
+      //  enemycreator('./img/ship4.png',context)
+
+       let img  = document.createElement('img')
+       img.src ='./img/explosion0.png'
+       img.onload = function () {
+        context.clearRect(0,0,1000,500) 
+        context.drawImage(img,0,0,150,100,0,0,70,70)
+         
+       }
+       
 }
 
 
 function enemycreator(src,somCtx) {
-        let enX =0
-        let enY =0
-  
-          setInterval(() => {
-            // enX+=100
-          let swetsher =true
-         let enemyimg =document.createElement('img')
-         enemyimg.src =src
-         enY+=5
-         enX+=1
-         enemyimg.onload= function () {
-          somCtx.clearRect(0,0,1000,500)
-           somCtx.drawImage(enemyimg,0,0,700,400,enX,enY,40,30)
+let enX =0
+let enY =0
+let swetsher =true
+setInterval(() => {
+let enemyimg =document.createElement('img')
+    enemyimg.src =src
+   // creat function to macke enemy move as cercel
+   
+    enemyimg.onload= function () {
+    somCtx.clearRect(0,0,1000,500)
+    somCtx.drawImage(enemyimg,0,0,700,400,enX,enY,40,30)
                  } 
-          if (enY==500 && swetsher==true) {
-          enY=0
-          enX+=50
+console.log(enY);
+console.log(swetsher);
+console.log(enX);
 
-            
-          }
-          else{if(enX==1000 && swetsher==true){
-            swetsher=false
-            enX-=50
-          } }
-        
-          
-        }, 50);
 
-        
-         
+  
+    if (enX==0) {
+      swetsher= true
+    }
+
+    if (enY==500&&swetsher==true) {
+      enY=0
+      enX+=50 
+    }else{
+      enY+=5
+   
+    }  
+
+
+    if(enX==1000){
+    swetsher=false } 
+
+    // if ( enY==500 && swetsher==false){
+    //   enY=0
+    //   enX-=50 
+    // }else{
+    //   enY+=5
+    //   }  
+        },50);
         }
+
         
         
       
